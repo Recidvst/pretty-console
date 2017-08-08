3ec633cd529aa5e7b4c4e05b307e6b40f12d34d5
Chris Snowden <cdsnowden@gmail.com>
2017-08-08 12:15:45 +0100

    init js | init method and switch

---
+++ console.js
@@ -0,0 +1,33 @@
+
+console.pretty = function(i) {
+  // for now, log
+  console.log(i);
+  
+  // get type of input
+  var inputType = typeof i;
+  
+  // different action dependng on type
+    
+  switch(inputType) {
+    case string :
+        break;
+    case array :
+        break;
+    case object :
+        if ( not null ) {
+            if ( one-level deep object ) {
+            }
+            else {
+            }
+        }
+        else {
+        }
+        break;        
+    case boolean :
+        break;     
+    case undefined :
+        break;
+    default :
+  }
+
+}
